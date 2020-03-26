3af43de5304006ebf1e183c773f83468a4a050c7
andressaggudelo <andresagudelo9239@gmail.com>
2020-03-26 02:18:33 -0500

    Bring and print sample data from JSONPlaceHolder API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 // This page will redirect to Home screen
 class Loading extends StatefulWidget {
@@ -7,6 +9,21 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(response.body); // This is a JSON string
+    print(data);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
